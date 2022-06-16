9149f2814dbd43e1579a7fd213498d964910fc96
Neo <fullstackneo@gmail.com>
2022-06-16 02:31:10 -0600

    refactor productlist to use global state

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList({ currentCategory }) {
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+  const { currentCategory } = state;
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  // const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  // const products = data?.products || [];
+
+  const { products } = state;
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+    }
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
       return products;
     }
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return products.filter(product => product.category._id === currentCategory);
   }
 
   return (
@@ -25,7 +41,7 @@ function ProductList({ currentCategory }) {
       <h2>Our Products:</h2>
       {products.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
+          {filterProducts().map(product => (
             <ProductItem
               key={product._id}
               _id={product._id}
