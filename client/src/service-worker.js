a3a1b84e028c707cbb773e2c27580dd4452debdc
Neo <fullstackneo@gmail.com>
2022-06-18 14:03:16 -0600

    test deploy

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -52,7 +52,7 @@ registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) =>
     url.origin === self.location.origin &&
-    url.pathname.endsWith('.png' || 'jpg'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    url.pathname.endsWith('.png' || '.jpg'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
